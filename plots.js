b48a3ef905b98082b47f323b7577769ac107494d
mengxiaomeng01 <mengxiao23@gmail.com>
2020-06-21 12:40:59 -0500

    editting files

--- plots.js
+++ plots.js
@@ -32,7 +32,6 @@ function buildMetadata(sample) {
   
       PANEL.html("");
       Object.entries(result).forEach(([key,value]) => {
-        console.log(key,value)
         PANEL.append("h6").text(key+": " + value);
       });
     });
@@ -106,21 +105,86 @@ function buildMetadata(sample) {
 
       Plotly.newPlot('bubble', [traceBubble], bubbleLayout);
 
+      console.log("I'm here")
+
       // Gauge Chart
-      var data = [
-        {
-          domain: { x: [0, 1], y: [0, 1] },
-          value: 270,
-          title: { text: "Speed" },
-          type: "indicator",
-          mode: "gauge+number"
-        }
-      ];
-      
-      var layout = { width: 600, height: 500, margin: { t: 0, b: 0 } };
-      
-      Plotly.newPlot('gauge', [data], layout);
-      
+
+      // Input wfreq
+      var metadata = data.metadata;
+      var resultArray = metadata.filter(sampleObj => sampleObj.id == sample);
+      var result = resultArray[0];
+      console.log(result)
+
+      var level = 20*result.wfreq;
+
+      // Trig to calc meter point
+      var degrees = 180 - level,
+          radius = .5;
+      var radians = degrees * Math.PI / 180;
+      var x = radius * Math.cos(radians);
+      var y = radius * Math.sin(radians);
+      var path1 = (degrees < 45 || degrees > 135) ? 'M -0.0 -0.025 L 0.0 0.025 L ' : 'M -0.025 -0.0 L 0.025 0.0 L ';
+      // Path: may have to change to create a better triangle
+      var mainPath = path1,
+          pathX = String(x),
+          space = ' ',
+          pathY = String(y),
+          pathEnd = ' Z';
+      var path = mainPath.concat(pathX,space,pathY,pathEnd);
+
+      var data = [{ type: 'scatter',
+        x: [0], y:[0],
+          marker: {size: 14, 
+            color:'850000',
+            colorscale:'YlGnBu'},
+          showlegend: false,
+          name: 'speed',
+          text: level,
+          hoverinfo: 'text+name'},
+        { values: [1,1,1,1,1,1,1,1,1,9],
+        rotation: 90,
+        text: ['8-9', '7-8', '6-7', '5-6', '4-5','3-4','2-3','1-2','0-1',''],
+        textinfo: 'text',
+        textposition:'inside',
+        marker: {
+          colors: [
+            "rgba(10, 100, 0, .5)",
+            "rgba(14, 127, 0, .5)",
+            "rgba(15, 140, 0, .5)",
+            "rgba(110, 154, 22, .5)",
+            "rgba(170, 202, 42, .5)",
+            "rgba(202, 209, 95, .5)",
+            "rgba(210, 206, 145, .5)",
+            "rgba(232, 226, 202, .5)",
+            "rgba(255, 255, 255, 0)"
+          ]
+        },
+        hoverinfo: 'label',
+        hole: .5,
+        type: 'pie',
+        showlegend: false
+      }];
+
+      var layout = {
+        shapes:[{
+            type: 'path',
+            path: path,
+            fillcolor: '850000',
+            line: {
+              color: '850000'
+            }
+          }],
+        height: 400,
+        width: 400,
+        title: '<b>'+'Belly Button Washing Frequency'+ '</b>' + "<br>" + 'Scrub per Week',
+        xaxis: {zeroline:false, showticklabels:false,
+                  showgrid: false, range: [-1, 1]},
+        yaxis: {zeroline:false, showticklabels:false,
+                  showgrid: false, range: [-1, 1]}
+      };
+
+      Plotly.newPlot('gauge', data, layout);
+        
     });
 
   };
